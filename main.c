90ad3bd03dcdaa0b6ca8d49f05d28ad7ad82abb8
JersonCh1 <163582930+JersonCh1@users.noreply.github.com>
2024-04-25 17:17:25 -0500

    Subiendo archivos, para mostrar el enlzamiento y compilacion.

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int dist(int, int);
+
+int main(int argc, char *argv[]) {
+  printf("El promedio 8 y 6 es: %d\n", dist(8, 6));
+  return 0;
+}
---
+++ otro.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+int dist(int a, int b)
+{
+    return(a+b)/2;
+}
